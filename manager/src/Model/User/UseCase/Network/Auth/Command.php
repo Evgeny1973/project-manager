208ca17feb9625093de139e333a5cd579208f3e6
Evgeny1973 <xonix75@mail.ru>
2019-10-08 17:14:57 +0300

    Model User ready

--- manager/src/Model/User/UseCase/Network/Auth/Command.php
+++ manager/src/Model/User/UseCase/Network/Auth/Command.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace App\Model\User\UseCase\Network;
+namespace App\Model\User\UseCase\Network\Auth;
 
 
 class Command
--- manager/src/Model/User/UseCase/Network/Auth/Handler.php
+++ manager/src/Model/User/UseCase/Network/Auth/Handler.php
@@ -1,13 +1,14 @@
 <?php
 
 
-namespace App\Model\User\UseCase\Network;
+namespace App\Model\User\UseCase\Network\Auth;
 
 
 use App\Model\Flusher;
 use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\User;
 use App\Model\User\Entity\User\UserRepository;
+use App\Model\User\UseCase\Network\Auth\Command;
 
 class Handler
 {
