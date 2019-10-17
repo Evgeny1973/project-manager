2f6abc8f22918326a07df1bcb60f4fed165320a7
Evgeny1973 <xonix75@mail.ru>
2019-10-17 23:29:19 +0300

    User login via facebook

--- manager/src/Model/User/UseCase/Network/Auth/Command.php
+++ manager/src/Model/User/UseCase/Network/Auth/Command.php
@@ -10,9 +10,13 @@ class Command
      * @var string
      */
     public $network;
-
     /**
      * @var string
      */
     public $identity;
+    public function __construct(string $network, string $identity)
+    {
+        $this->network = $network;
+        $this->identity = $identity;
+    }
 }
---
+++ manager/tests/Functional/HomeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Tests\Functional;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+    public function testGuest()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+        $this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
+    }
+
+    public function testSuccess(): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'admin@test.com',
+            'PHP_AUTH_PW' => '123456',
+        ]);
+        $crawler = $client->request('GET', '/');
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Hello', $crawler->filter('h1')->text());
+    }
+}
