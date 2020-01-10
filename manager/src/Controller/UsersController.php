d2befc978c94412ef598c05020e7ff806bcc8c8f
Evgeny1973 <xonix75@mail.ru>
2020-01-11 01:50:04 +0300

    Added pagination and sorting

--- manager/src/Controller/UsersController.php
+++ manager/src/Controller/UsersController.php
@@ -20,7 +20,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
- * @Route("/users")
+ * @Route("/users", name="users")
  */
 class UsersController extends AbstractController
 {
@@ -37,7 +37,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("", name="users")
+     * @Route("", name="")
      * @param Request $request
      * @param UserFetcher $fetcher
      * @return Response
@@ -64,7 +64,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/create", name="users.create")
+     * @Route("/create", name=".create")
      * @param Request $request
      * @param Create\Handler $handler
      * @return Response
@@ -91,7 +91,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="users.edit")
+     * @Route("/{id}/edit", name=".edit")
      * @param User $user
      * @param Request $request
      * @param Edit\Handler $handler
@@ -99,6 +99,11 @@ class UsersController extends AbstractController
      */
     public function edit(User $user, Request $request, Edit\Handler $handler): Response
     {
+        if ($user->getId()->getValue() === $this->getUser()->getId()) {
+            $this->addFlash('error', 'Нельзя редактировать свои данные.');
+            return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+        }
+
         $command = Edit\Command::fromUser($user);
 
         $form = $this->createForm(Edit\Form::class, $command);
@@ -120,7 +125,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/role", name="users.role")
+     * @Route("/{id}/role", name=".role")
      * @param User $user
      * @param Request $request
      * @param Role\Handler $handler
@@ -154,7 +159,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/confirm", name="users.confirm", methods={"POST"})
+     * @Route("/{id}/confirm", name=".confirm", methods={"POST"})
      * @param User $user
      * @param Request $request
      * @param Confirm\Manual\Handler $handler
@@ -176,7 +181,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/activate", name="users.activate", methods={"POST"})
+     * @Route("/{id}/activate", name=".activate", methods={"POST"})
      * @param User $user
      * @param Request $request
      * @param Activate\Handler $handler
@@ -201,7 +206,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/block", name="users.block", methods={"POST"})
+     * @Route("/{id}/block", name=".block", methods={"POST"})
      * @param User $user
      * @param Request $request
      * @param Block\Handler $handler
@@ -231,7 +236,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="users.show")
+     * @Route("/{id}", name=".show")
      * @param User $user
      * @return Response
      */
