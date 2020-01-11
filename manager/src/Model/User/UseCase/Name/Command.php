b5eda6e6ce100b4e88f401acf2c29088aea73546
Evgeny1973 <xonix75@mail.ru>
2020-01-12 01:22:23 +0300

    getDetail() changed to get()

--- manager/src/Model/User/UseCase/Name/Command.php
+++ manager/src/Model/User/UseCase/Name/Command.php
@@ -3,6 +3,7 @@
 
 namespace App\Model\User\UseCase\Name;
 
+use App\Model\User\Entity\User\User;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
@@ -29,5 +30,13 @@ class Command
     public function __construct(string $id)
     {
         $this->id = $id;
+    }
+
+    public static function fromUser(User $user)
+    {
+        $command = new self($user->getId()->getValue());
+        $command->firstName = $user->getName()->getFirst();
+        $command->lastName = $user->getName()->getLast();
+        return $command;
     }
 }
