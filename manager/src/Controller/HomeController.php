6cbb8e155e570d2e6f7c16e9f43faf699e78d4da
Evgeny1973 <xonix75@mail.ru>
2020-01-13 00:30:42 +0300

    Work members group

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -6,15 +6,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/work", name="work")
+     * @Route("/", name="home")
      * @return Response
      */
     public function index(): Response
     {
-        return $this->redirectToRoute('work.members.groups');
+        return $this->render('app/home.html.twig');
     }
 }
