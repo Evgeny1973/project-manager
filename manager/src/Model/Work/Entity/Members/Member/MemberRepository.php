6941a57a5c2c0420af8c24904c89918f741c0313
Evgeny1973 <xonix75@mail.ru>
2020-01-21 23:56:57 +0300

    Protect removing non-empty group

--- manager/src/Model/Work/Entity/Members/Member/MemberRepository.php
+++ manager/src/Model/Work/Entity/Members/Member/MemberRepository.php
@@ -6,6 +6,7 @@ namespace App\Model\Work\Entity\Members\Member;
 
 use App\Model\EntityNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Model\Work\Entity\Members\Group\Id as GroupId;
 
 class MemberRepository
 {
@@ -23,10 +24,19 @@ class MemberRepository
 
     public function has(Id $id): bool
     {
-        return $this->repo->CreateQueryBuilder('t')
+        return $this->repo->createQueryBuilder('t')
                 ->select('COUNT(t.id)')
                 ->andWhere('t.id = :id')
                 ->setParameter(':id', $id->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function hasByGroup(GroupId $id): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.group = :group')
+                ->setParameter(':group', $id->getValue())
                 ->getQuery()->getSingleScalarResult() > 0;
     }
 
--- manager/src/Model/Work/UseCase/Members/Group/Remove/Handler.php
+++ manager/src/Model/Work/UseCase/Members/Group/Remove/Handler.php
@@ -8,6 +8,7 @@ use App\Model\Flusher;
 use App\Model\Work\Entity\Members\Group\Group;
 use App\Model\Work\Entity\Members\Group\GroupRepository;
 use App\Model\Work\Entity\Members\Group\Id;
+use App\Model\Work\Entity\Members\Member\MemberRepository;
 
 class Handler
 {
@@ -20,15 +21,26 @@ class Handler
      */
     private $flusher;
 
-    public function __construct(GroupRepository $groups, Flusher $flusher)
+    /**
+     * @var MemberRepository
+     */
+    private $members;
+
+    public function __construct(GroupRepository $groups, MemberRepository $members, Flusher $flusher)
     {
         $this->groups = $groups;
         $this->flusher = $flusher;
+        $this->members = $members;
     }
 
     public function handle(Command $command): void
     {
         $group = $this->groups->get(new Id($command->id));
+
+        if ($this->members->hasByGroup($group->getId())) {
+            throw new \DomainException('Группа не пустая.');
+        }
+
         $this->groups->remove($group);
         $this->flusher->flush();
     }
