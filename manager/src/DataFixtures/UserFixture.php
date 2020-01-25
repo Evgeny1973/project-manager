d730695f7ca5ffe7b92bb5d643953dd952c54459
Evgeny1973 <xonix75@mail.ru>
2020-01-25 23:56:04 +0300

    Update Fixtures

--- manager/src/DataFixtures/UserFixture.php
+++ manager/src/DataFixtures/UserFixture.php
@@ -15,6 +15,10 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class UserFixture extends Fixture
 {
+
+    public const REFERENCE_ADMIN = 'user_user_admin';
+    public const REFERENCE_USER = 'user_user_user';
+
     /**
      * @var PasswordHasher
      */
@@ -55,6 +59,7 @@ class UserFixture extends Fixture
             $hash
         );
         $manager->persist($confirmed);
+        $this->setReference(self::REFERENCE_USER, $confirmed);
 
         $admin = $this->createAdminByEmail(
             new Name('James', 'Bond'),
@@ -62,6 +67,7 @@ class UserFixture extends Fixture
             $hash
         );
         $manager->persist($admin);
+        $this->setReference(self::REFERENCE_ADMIN, $admin);
 
         $manager->flush();
     }
