c1e5e9dcb0c7d2e2dd4625321525e9cb8aa79db6
Evgeny1973 <xonix75@mail.ru>
2020-01-30 21:14:43 +0300

    Department create usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Project/Department/Create/Command.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Model\Work\UseCase\Projects\Project\Department\Create;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+
+    public $project;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $name;
+
+    public function __construct(string $project)
+    {
+        $this->project = $project;
+    }
+}
