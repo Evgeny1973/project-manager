2b9f1c2b46068ec57b7c72d35e0bb20439c94ed7
Evgeny1973 <xonix75@mail.ru>
2020-01-30 23:24:51 +0300

    Roles Id IdType

---
+++ manager/src/Model/Work/Entity/Projects/Role/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Model\Work\Entity\Projects\Role;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="work_project_roles")
+ */
+class Role
+{
+
+}
