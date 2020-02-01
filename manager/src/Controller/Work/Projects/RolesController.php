41d9b9decffa545598cf415a8c905c9ac4e87437
Evgeny1973 <xonix75@mail.ru>
2020-02-01 15:25:00 +0300

    Roles controller

--- manager/src/Controller/Work/Projects/RolesController.php
+++ manager/src/Controller/Work/Projects/RolesController.php
@@ -3,10 +3,17 @@
 
 namespace App\Controller\Work\Projects;
 
-
+use App\Model\Work\Entity\Projects\Role\Permission;
+use App\Model\Work\Entity\Projects\Role\Role;
+use App\Model\Work\UseCase\Projects\Role\Copy;
+use App\Model\Work\UseCase\Projects\Role\Create;
+use App\Model\Work\UseCase\Projects\Role\Edit;
+use App\Model\Work\UseCase\Projects\Role\Remove;
+use App\ReadModel\Work\Projects\RoleFetcher;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,8 +34,16 @@ class RolesController extends AbstractController
         $this->logger = $logger;
     }
 
-    public function index(): Response
+    /**
+     * @Route("", name="")
+     * @param RoleFetcher $fetcher
+     * @return Response
+     */
+    public function index(RoleFetcher $fetcher): Response
     {
+        $roles = $fetcher->all();
+        $permissions = Permission::names();
 
+        return $this->render('app/work/projects/roles/index.html.twig', ['roles' => $roles, 'permissions' => $permissions]);
     }
 }
