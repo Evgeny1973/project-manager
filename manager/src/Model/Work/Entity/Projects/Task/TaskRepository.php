d4e7bb10472c14be0a39035959d016edcdbce7aa
Evgeny1973 <xonix75@mail.ru>
2020-02-15 20:38:39 +0300

    add task repository

---
+++ manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Model\Work\Entity\Projects\Task;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskRepository
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+    private $connection;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->connection = $em->getConnection();
+    }
+
+    public function add(Task $task): void
+    {
+        $this->em->persist($task);
+    }
+
+    public function nexId(): Id
+    {
+        return new Id((int)$this->connection->query('SELECT nextval(\'work_projects_tasks_seq\')')->fetchColumn());
+    }
+}
