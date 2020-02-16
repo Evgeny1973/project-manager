e768dfeefc0ac9eaefe07f8a834b3b92bd134865
Evgeny1973 <xonix75@mail.ru>
2020-02-16 20:01:13 +0300

    add task priority usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Priority/Form.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Model\Work\UseCase\Projects\Task\Priority;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('priority', ChoiceType::class, ['choices' =>
+                [
+                    'Low' => 1,
+                    'Normal' => 2,
+                    'High' => 3,
+                    'Extra' => 4
+                ], 'attr' => ['onchange' => 'this.form.submit()']]);
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class,
+        ]);
+    }
+}
