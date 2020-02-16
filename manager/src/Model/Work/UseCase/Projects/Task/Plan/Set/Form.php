2223d60854bda47961184d7418cf7b78fd35885f
Evgeny1973 <xonix75@mail.ru>
2020-02-16 19:18:12 +0300

    add task plan set usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Plan/Set/Form.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Model\Work\UseCase\Projects\Task\Plan\Set;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Form extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable'
+            ]);
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class,
+        ]);
+    }
+    
+}
