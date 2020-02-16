18eeaa5ab8fc19db9c74663912c24f9dc82c2210
Evgeny1973 <xonix75@mail.ru>
2020-02-16 16:57:09 +0300

    add task usecase edit

--- manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
+++ manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -4,6 +4,7 @@
 namespace App\Model\Work\Entity\Projects\Task;
 
 
+use App\Model\EntityNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
 
 class TaskRepository
@@ -13,11 +14,22 @@ class TaskRepository
      */
     private $em;
     private $connection;
+    private $repo;
 
     public function __construct(EntityManagerInterface $em)
     {
+        $this->repo = $em->getRepository(Task::class);
         $this->em = $em;
         $this->connection = $em->getConnection();
+    }
+
+    public function get(Id $id): Task
+    {
+        /** @var Task $task */
+        if (!$task = $this->repo->find($id)) {
+            throw new EntityNotFoundException('Задача не найдена.');
+        }
+        return $task;
     }
 
     public function add(Task $task): void
