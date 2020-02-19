62e01a0c273858f0e1b51b415102aa4fc5083a9f
Evgeny1973 <xonix75@mail.ru>
2020-02-20 00:57:24 +0300

    new migrations

---
+++ manager/src/Migrations/Version20200219202317.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200219202317 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE work_projects_project_departments ALTER project_id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_project_departments ALTER project_id DROP DEFAULT');
+        $this->addSql('ALTER TABLE work_projects_projects ALTER id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_projects ALTER id DROP DEFAULT');
+        $this->addSql('ALTER TABLE work_projects_project_memberships ALTER project_id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_project_memberships ALTER project_id DROP DEFAULT');
+        $this->addSql('ALTER TABLE work_projects_tasks ALTER project_id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_tasks ALTER project_id DROP DEFAULT');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE work_projects_projects ALTER id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_projects ALTER id DROP DEFAULT');
+        $this->addSql('ALTER TABLE work_projects_project_departments ALTER project_id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_project_departments ALTER project_id DROP DEFAULT');
+        $this->addSql('ALTER TABLE work_projects_project_memberships ALTER project_id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_project_memberships ALTER project_id DROP DEFAULT');
+        $this->addSql('ALTER TABLE work_projects_tasks ALTER project_id TYPE UUID');
+        $this->addSql('ALTER TABLE work_projects_tasks ALTER project_id DROP DEFAULT');
+    }
+}
