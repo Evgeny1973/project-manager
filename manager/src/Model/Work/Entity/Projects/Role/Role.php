2257fc83912becdefc2bad3cf9f1a67a28ca35e7
Evgeny1973 <xonix75@mail.ru>
2020-02-19 23:16:38 +0300

    fix role entity

--- manager/src/Model/Work/Entity/Projects/Role/Role.php
+++ manager/src/Model/Work/Entity/Projects/Role/Role.php
@@ -1,14 +1,14 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Model\Work\Entity\Projects\Role;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use function foo\func;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity
  * @ORM\Table(name="work_projects_roles")
  */
 class Role
@@ -16,27 +16,25 @@ class Role
     /**
      * @var Id
      * @ORM\Column(type="work_projects_role_id")
-     * @ORM\Id()
+     * @ORM\Id
      */
     private $id;
-
     /**
      * @var string
      * @ORM\Column(type="string", unique=true)
      */
     private $name;
-
     /**
-     * @var ArrayCollection | Permission[]
+     * @var ArrayCollection|Permission[]
      * @ORM\Column(type="work_projects_role_permissions")
      */
     private $permissions;
-
+    
     /**
      * Role constructor.
      * @param Id $id
      * @param string $name
-     * @param array $permissions
+     * @param string[] $permissions
      */
     public function __construct(Id $id, string $name, array $permissions)
     {
@@ -44,53 +42,52 @@ class Role
         $this->name = $name;
         $this->setPermissions($permissions);
     }
-
+    
+    /**
+     * @param string $name
+     * @param string[] $permissions
+     */
     public function edit(string $name, array $permissions): void
     {
         $this->name = $name;
         $this->setPermissions($permissions);
     }
-
+    
     public function hasPermission(string $permission): bool
     {
         return $this->permissions->exists(static function (Permission $current) use ($permission) {
             return $current->isNameEqual($permission);
         });
     }
-
+    
     public function clone(Id $id, string $name): self
     {
-        return new self($id, $name, $this->permissions->toArray());
+        return new self($id, $name, array_map(static function (Permission $permission) {
+            return $permission->getName();
+        }, $this->permissions->toArray()));
     }
-
-    /**
-     * @return Id
-     */
+    
     public function getId(): Id
     {
         return $this->id;
     }
-
-    /**
-     * @return string
-     */
+    
     public function getName(): string
     {
         return $this->name;
     }
-
+    
     /**
      * @return Permission[]
      */
-    public function getPermissions()
+    public function getPermissions(): array
     {
         return $this->permissions->toArray();
     }
-
-
+    
     public function setPermissions(array $names): void
     {
-        $this->permissions = new ArrayCollection(array_map(static function(string $name) {
+        $this->permissions = new ArrayCollection(array_map(static function (string $name) {
             return new Permission($name);
         }, array_unique($names)));
     }
