02fd4f6da09a2d9c92fe90b7ed8d112a21cd58cd
Evgeny1973 <xonix75@mail.ru>
2020-02-19 16:43:53 +0300

    add form prefix

--- manager/src/Model/Work/UseCase/Projects/Task/Progress/Form.php
+++ manager/src/Model/Work/UseCase/Projects/Task/Progress/Form.php
@@ -33,4 +33,9 @@ class Form extends AbstractType
             'data_class ' => Command::class,
         ]);
     }
+    
+    public function getBlockPrefix(): string
+    {
+        return 'progress';
+    }
 }
