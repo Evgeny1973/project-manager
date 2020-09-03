5aeb5fc33379fbbd34360cdd5c1a1ef07c9a85e0
Evgeny1973 <xonix75@mail.ru>
2020-09-04 01:46:48 +0300

    add centrifugo

--- manager/assets/js/app.js
+++ manager/assets/js/app.js
@@ -2,3 +2,14 @@ require('../css/app.scss');
 
 require('bootstrap');
 require('@coreui/coreui');
+
+const Centrifuge = require('centrifuge');
+
+document.addEventListener('DOMContentLoaded', function () {
+    let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
+    const centrifuge = new Centrifuge(url);
+    centrifuge.subscribe('alerts', function (message) {
+        console.log(message.data.message);
+    });
+    centrifuge.connect();
+});
