9227889d2858c1cf44bb2eeeb7c8d30cf334fdc3
Evgeny1973 <xonix75@mail.ru>
2020-09-05 01:35:29 +0300

    massage via centrifugo for user

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use phpcent\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index(): Response
+    public function index(Client $centrifugo): Response
     {
+        $centrifugo->publish('alerts', ['message' => 'PHP!']);
+        
         return $this->render('app/home.html.twig');
     }
 }
