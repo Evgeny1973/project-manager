47d3e960cc06c634ae57683773de74ec3f8f6a2a
Evgeny1973 <xonix75@mail.ru>
2020-09-07 01:14:04 +0300

    add tests for api

---
+++ manager/src/Controller/Api/Work/Projects/ProjectsController.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api\Work\Projects;
+
+final class ProjectsController
+{
+    
+}
--- manager/src/Security/OAuth/Server/UserResolver.php
+++ manager/src/Security/OAuth/Server/UserResolver.php
@@ -4,7 +4,54 @@ declare(strict_types=1);
 
 namespace App\Security\OAuth\Server;
 
-final class UserResolver
+use App\Model\User\Service\PasswordHasher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Trikoder\Bundle\OAuth2Bundle\Event\UserResolveEvent;
+use Trikoder\Bundle\OAuth2Bundle\OAuth2Events;
+
+final class UserResolver implements EventSubscriberInterface
 {
+    /**
+     * @var UserProviderInterface
+     */
+    private $userProvider;
     
+    /**
+     * @var PasswordHasher
+     */
+    private $hasher;
+    
+    public function __construct(UserProviderInterface $userProvider, PasswordHasher $hasher)
+    {
+        $this->userProvider = $userProvider;
+        $this->hasher = $hasher;
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return [
+            OAuth2Events::USER_RESOLVE => 'onUserResolve',
+        ];
+    }
+    
+    public function onUserResolve(UserResolveEvent $event)
+    {
+        $user = $this->userProvider->loadUserByUsername($event->getUsername());
+        
+        if (null === $user) {
+            return;
+        }
+        
+        if (!$user->getPassword()) {
+            return;
+        }
+        
+        if (!$this->hasher->validate($event->getPassword(), $user->getPassword())) {
+            return;
+        }
+        
+        $event->setUser($user);
+    }
 }
